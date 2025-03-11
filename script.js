2532f9883d623838a5bae4bd1a2c584e8f67a095
jjufelix <julia.s.felix7@aluno.senai.br>
2025-03-11 14:38:48 -0300

    atulização

---
+++ script.js
@@ -0,0 +1,38 @@
+function initTabNav() {
+    const tabMenu= document.querySelectorAll('.js-tabmenu li');  
+    const tabContent= document.querySelectorAll('.js-tacontent section')
+
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo'); 
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+
+            tabContent[index].classList.add('ativo')
+
+        }
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click',()=> {
+                activeTab(index)
+            });
+        });
+
+    }
+
+}
+initTabNav();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
