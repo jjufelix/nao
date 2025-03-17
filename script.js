6b7d6b12a793a8cbcdfd405bd1fd4979c178a451
jjufelix <julia.s.felix7@aluno.senai.br>
2025-03-17 10:32:59 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 function initTabNav() {
-    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabMenu = document.querySelectorAll('.js-menu button');
     const tabContent = document.querySelectorAll('.js-tabcontent section');
+    const imagens = document.querySelectorAll('.js-imagens img');
   
-    if(tabMenu.length && tabContent.length) {
-      tabContent[0].classList.add('ativo');
+    if (tabMenu.length && tabContent.length && imagens.length) {
+        tabContent[0].classList.add('ativo');
+        imagens[0].classList.add('ativo');
   
-      function activeTab(index) {
-        tabContent.forEach((section) => {
-          section.classList.remove('ativo');
+        function activeTab(index) {
+            tabContent.forEach((section) => section.classList.remove('ativo'));
+            imagens.forEach((img) => img.classList.remove('ativo'));
+  
+            tabContent[index].classList.add('ativo');
+            imagens[index].classList.add('ativo');
+        }
+  
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
-      }
     }
   }
+  
   initTabNav();
+  
